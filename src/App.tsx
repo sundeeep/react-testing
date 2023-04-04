d44f4b3deee7cf5e6a13ae4482f8997a9210032d
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-04-05 00:51:44 +0530

    test driven development ch01

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Greet } from './components/greet/greet';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Greet />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/components/greet/greet.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Greet = () => {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
-}
